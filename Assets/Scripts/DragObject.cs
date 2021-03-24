b092e81a3f8804753de841ea161d894eec506f82
ricky99er <70237944+ricky99er@users.noreply.github.com>
2021-03-25 01:02:29 +0530

    Add files via upload

---
+++ Assets/Scripts/DragObject.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragObject : MonoBehaviour
+{
+
+    public SkinnedMeshRenderer skinedRenderer;
+    public int blendShapeIndex;
+
+    private Vector3 mOffset;
+    private float mZCoord;
+
+    void OnMouseDown()
+    {
+
+        mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+        mOffset = GetMouseAsWorldPoint();
+    }
+
+    private Vector3 GetMouseAsWorldPoint()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+        mousePoint.z = mZCoord;
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+
+    void OnMouseDrag()
+    {
+        float x = Vector3.Distance(GetMouseAsWorldPoint(), transform.position);
+        float y = Vector3.Distance(mOffset, transform.position);
+
+        float w = skinedRenderer.GetBlendShapeWeight(blendShapeIndex);
+
+        if (x > y)
+        {
+            skinedRenderer.SetBlendShapeWeight(blendShapeIndex, Mathf.Clamp(w - 1f, 0, 100));
+        }
+        else if (x < y)
+        {
+            skinedRenderer.SetBlendShapeWeight(blendShapeIndex, Mathf.Clamp(w + 1f, 0, 100));
+        }
+    }
+
+}
