1d54935f2ff3b2be809b90b670e52f4ed416c158
ricky99er <70237944+ricky99er@users.noreply.github.com>
2021-03-25 00:57:53 +0530

    Add files via upload

---
+++ Assets/Input.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+public class Input : MonoBehaviour
+{
+    public gameObject Camera mOffset
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 input = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstickl);
+        Vector3 movement = camera.transform.TransformDirection(input.x, 0, input.y);
+        movement.y = 0;
+        movement = movement.magnitude == 0 ? Vector3.zero : movement / movement.magnitude;
+        
+    }
+}
